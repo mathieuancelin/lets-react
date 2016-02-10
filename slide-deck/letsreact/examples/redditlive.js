22d03028202fbf12146524b36d6ed46e98f9162a
Mathieu <mathieu.ancelin@gmail.com>
2016-02-10 09:09:31 +0100

    Add tips

--- slide-deck/letsreact/examples/redditlive.js
+++ slide-deck/letsreact/examples/redditlive.js
@@ -49,6 +49,7 @@ const Styles = {
   }
 };
 function fetchSubreddit(subreddit) {
+  // onClick setState props thumbnail preview componentDidMount selectSelf select 
   // return fetch(`http://localhost:3000/backupreddit/reddit.json`).then((r) => r.json()).then((response) => {
   return fetch(`http://www.reddit.com/r/${subreddit}.json?sort=top&t=month`).then((r) => r.json()).then((response) => {
     return response.data.children
